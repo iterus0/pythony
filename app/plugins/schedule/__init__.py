7ad9c9e3c046e22da4a8397edbf4a889216440ab
Iterus <supermax705@gmail.com>
2017-08-09 14:11:06 +0300

    Небольшая реорганизация функции format_lesson

--- app/plugins/schedule/__init__.py
+++ app/plugins/schedule/__init__.py
@@ -10,28 +10,34 @@ db_name = 'schedule.db'
 db_path = os.path.join(dir_path, db_name)
 
 
-def format_lesson(db, lesson):
+def remaining_time(hour, minute):
+    current_time = datetime.datetime.now()
+    current_date = current_time.date()
+    finish_time = datetime.time(hour=hour, minute=minute)
+    finish_dt = datetime.datetime.combine(current_date, finish_time)
+    remaining_time = finish_dt - current_time
+    remaining_mins = math.ceil(remaining_time.total_seconds() / 60)
+
+    return str(remaining_mins) + ' минут'
+
+
+def format_lesson(lesson):
+    db = SQLighter(db_path)
+
     l_name = db.subject_name(lesson[0])
     l_teacher = db.teacher_full_name(lesson[1])
     l_aud = lesson[2]
-
-    # черная магия со временем
-    current_time = datetime.datetime.now()
-    current_date = current_time.date()
     finish_hour, finish_minute = [int(n) for n in lesson[4].split(':')]
-    finish_time = datetime.time(hour=finish_hour, minute=finish_minute)
-    finish_dt = datetime.datetime.combine(current_date, finish_time)
-    remaining_time = finish_dt - current_time
+    l_left = remaining_time(finish_hour, finish_minute)
 
-    l_left = math.ceil(remaining_time.total_seconds() / 60)
-
+    db.close()
     return ('Предмет: {name}\n'
             'Аудитория: {aud}\n'
             'Препод: {teacher}\n'
-            'До конца пары осталось {left} минут').format(name=l_name,
-                                                          aud=l_aud,
-                                                          teacher=l_teacher,
-                                                          left=l_left)
+            'До конца пары осталось {left}').format(name=l_name,
+                                                    aud=l_aud,
+                                                    teacher=l_teacher,
+                                                    left=l_left)
 
 
 @bot.message_handler(commands=["rasp"])
@@ -45,7 +51,7 @@ def get_current_lesson(message):
         msg += 'Сейчас пары нет'
 
     elif len(lessons) == 1:
-        msg += format_lesson(db, lessons[0])
+        msg += format_lesson(lessons[0])
 
     else:
         msg += '[!] Произошла ошибка'
