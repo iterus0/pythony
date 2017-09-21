f4e5c95ade9243226cca47634fa1bfd49dc5fe66
Iterus <supermax705@gmail.com>
2017-09-21 15:20:17 +0300

    Исправлено периодическое падение бота. (В будущем следует перейти на веб хуки)

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 from app import bot
+import time
+
+
+def main():
+    try:
+        bot.polling(none_stop=True)
+
+    except Exception as e:
+        print(e)
+        time.sleep(5)
+
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    main()
